929d6847f62e725da2a77950b9e59afae79d6c3e
Álvaro Gonçalves <alvaro.goncalves@cps.sp.gov.br>
2020-10-02 14:05:07 -0300

    Base de layout

---
+++ projeto01/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request, render_template, url_for
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+  name = request.args.get("name", None)
+  return render_template("index.html", name=name)
+
+
+@app.route("/api/<int:id>")
+def sensor(id):
+  return "Este é o sensor " + str(id)
+

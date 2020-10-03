6f45ce117ebec90a039266b313590f9fe067d9ff
Alejandro Druetta <aledruetta@gmail.com>
2020-10-03 11:55:48 -0300

    Fix indentation

--- projeto01/main.py
+++ projeto01/main.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-  name = request.args.get("name", None)
-  return render_template("index.html", name=name)
+    name = request.args.get("name", None)
+    return render_template("index.html", name=name)
 
 
 @app.route("/api/<int:id>")
 def sensor(id):
-  return "Este é o sensor " + str(id)
-
+    return "Este é o sensor " + str(id)

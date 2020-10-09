06d83043897269779fa6a3c4643a82d2b8db0f0b
Alejandro Druetta <aledruetta@gmail.com>
2020-10-09 12:41:11 -0300

    Flask RESTful

--- proj05_restful/projeto/ext/api/views.py
+++ proj05_restful/projeto/ext/api/views.py
@@ -1,69 +1,55 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request
+from flask_restful import Resource
+
 from projeto.ext.db import db
 from .models import Estacao
 
 bp = Blueprint("api", __name__)
 
 
-@bp.route("/")
-def index():
-    return render_template("index.html")
+class ApiRest(Resource):
+    def get(self):
+        estacoes = Estacao.query.all()
+        data = [estacao.json() for estacao in estacoes]
+        return {"estacoes": data}
+
+    def post(self):
+        data = request.get_json()
+
+        estacao = Estacao(
+            local=data["local"],
+            latitude=data["latitude"],
+            longitude=data["longitude"],
+        )
+
+        db.session.add(estacao)
+        db.session.commit()
+
+        return {"msg": "Success!"}
 
 
-# listar estacoes
-@bp.route("/api")
-def list():
-    estacoes = Estacao.query.all()
-    data = [estacao.json() for estacao in estacoes]
-    return {"estacoes": data}
+class ApiRestId(Resource):
+    def get(self, id):
+        estacao = Estacao.query.get(id)
+        return {"estacao": estacao.json()}
 
+    def put(self, id):
+        data = request.get_json()
+        estacao = Estacao.query.get(id)
 
-# apresentar uma estacao pelo id
-@bp.route("/api/<int:id>")
-def read(id):
-    estacao = Estacao.query.get(id)
-    return {"estacao": estacao.json()}
+        estacao.local = data.get("local", estacao.local)
+        estacao.latitude = data.get("latitude", estacao.latitude)
+        estacao.longitude = data.get("longitude", estacao.longitude)
 
+        db.session.add(estacao)
+        db.session.commit()
 
-# criar estacao
-@bp.route("/api", methods=["POST"])
-def create():
-    data = request.get_json()
+        return {"msg": estacao.json()}
 
-    estacao = Estacao(
-        local=data["local"],
-        latitude=data["latitude"],
-        longitude=data["longitude"],
-    )
+    def delete(self, id):
+        estacao = Estacao.query.get(id)
 
-    db.session.add(estacao)
-    db.session.commit()
+        db.session.delete(estacao)
+        db.session.commit()
 
-    return {"msg": "Success!"}
-
-
-# atualizar dados estacao pelo id
-@bp.route("/api/<int:id>", methods=["POST"])
-def update(id):
-    data = request.get_json()
-    estacao = Estacao.query.get(id)
-
-    estacao.local = data.get("local", estacao.local)
-    estacao.latitude = data.get("latitude", estacao.latitude)
-    estacao.longitude = data.get("longitude", estacao.longitude)
-
-    db.session.add(estacao)
-    db.session.commit()
-
-    return {"msg": estacao.json()}
-
-
-# deletar estacao pelo id
-@bp.route("/api/<int:id>/del", methods=["POST"])
-def delete(id):
-    estacao = Estacao.query.get(id)
-
-    db.session.delete(estacao)
-    db.session.commit()
-
-    return {"msg": "Success!"}
+        return {"msg": "Success!"}

1d2a3b6cf1d89b124b8434df6c023027a0cba613
Alejandro Druetta <aledruetta@gmail.com>
2020-10-16 11:09:13 -0300

    Add logout view

--- proj07_wtforms/projeto/ext/auth/views.py
+++ proj07_wtforms/projeto/ext/auth/views.py
@@ -3,7 +3,8 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
+from werkzeug.security import safe_str_cmp
 
 from .models import User
 
@@ -20,9 +21,12 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        print(user)
-        login_user(user)
-        return redirect(url_for("site.index"))
+        if user and safe_str_cmp(
+            form.password.data.encode("utf-8"),
+            user.password.encode("utf-8")
+        ):
+            login_user(user)
+            return redirect(url_for("site.index"))
     return render_template("login.html", form=form)
 
 
@@ -31,6 +35,8 @@ def signup():
     pass
 
 
-@bp.route("/logout", methods=["GET", "POST"])
+@bp.route("/logout")
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for("site.index"))

883ecd30afc0038e8012b2519e30beafd3945883
Alejandro Druetta <aledruetta@gmail.com>
2020-10-20 12:51:12 -0300

    Email validators

--- proj07_wtforms/projeto/ext/auth/views.py
+++ proj07_wtforms/projeto/ext/auth/views.py
@@ -1,21 +1,25 @@
 from passlib.hash import sha256_crypt
-from flask import Blueprint, flash, redirect, render_template, url_for
+from flask import Blueprint, flash, redirect, render_template, url_for, request
 from flask_wtf import FlaskForm
 from passlib.hash import sha256_crypt
 from wtforms import PasswordField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 from flask_login import login_user, login_required, logout_user
 
-from .models import User
 from projeto.ext.db import db
+from .models import User
 
 bp = Blueprint("auth", __name__)
 
 
 class LoginForm(FlaskForm):
-    email = EmailField("email", validators=[DataRequired()])
-    password = PasswordField("password", validators=[DataRequired()])
+    email = EmailField(
+        "email", validators=[DataRequired(),
+                             Email(check_deliverability=True)])
+    password = PasswordField("password",
+                             validators=[DataRequired(),
+                                         Length(min=8)])
 
 
 @bp.route("/login", methods=["GET", "POST"])
@@ -23,8 +27,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and sha256_crypt.verify(form.password.data,
-                                        user.password):
+        if user and sha256_crypt.verify(form.password.data, user.password):
             login_user(user)
             return redirect(url_for("site.index"))
         flash("Dados inválidos!")
@@ -40,6 +43,8 @@ def signup():
         db.session.add(user)
         db.session.commit()
         return redirect(url_for("site.index"))
+    if request.method == "POST":
+        flash("Dados inválidos!")
     return render_template("signup.html", form=form)
 
 

588d06608042c1c9916ebecfc301c26bccf3112f
Alejandro Druetta <aledruetta@gmail.com>
2020-10-27 10:54:07 -0300

    Update sensor simulator

--- proj08_visualizacoes/sensor_sim.py
+++ proj08_visualizacoes/sensor_sim.py
@@ -8,7 +8,11 @@ import numpy as np
 auth = {"email": "alvaro@gmail.com", "password": "12345678"}
 URL = "http://localhost:5000/token"
 response = requests.post(URL, json=auth)
-token = response.json()['access_token']
+TOKEN = response.json()['access_token']
+
+MIN_VAR = -0.5
+MAX_VAR = 0.5
+LINSPACE = np.linspace(MIN_VAR, MAX_VAR, 10)
 
 sensor = {"id": 1, "parametros": {"temp": [10, 38], "umidade": [50, 90]}}
 
@@ -18,11 +22,11 @@ for param in sensor["parametros"].keys():
     sensor["parametros"][param].append(random() * (pmax - pmin) + pmin)
 
 while True:
+
     for param in sensor["parametros"]:
         url = f"http://localhost:5000/api/v1.1/sensor/{sensor['id']}/{param}"
 
-        sensor["parametros"][param][2] += random() * choice(
-            np.linspace(-0.2, 0.2, 10))
+        sensor["parametros"][param][2] += random() * choice(LINSPACE)
 
         leitura = {
             "valor": f"{sensor['parametros'][param][2]:.2f}",
@@ -31,7 +35,7 @@ while True:
 
         response = requests.post(url,
                                  json=leitura,
-                                 headers={"Authorization": f"jwt {token}"})
+                                 headers={"Authorization": f"jwt {TOKEN}"})
         sleep(1)
 
         print(response.json())

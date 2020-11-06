301603b883c3e9cf6018b7de05187c91b2f76b46
Alejandro Druetta <aledruetta@gmail.com>
2020-11-06 12:11:27 -0300

    ADD grafico inial

--- proj08_visualizacoes/projeto/static/visualizacoes.js
+++ proj08_visualizacoes/projeto/static/visualizacoes.js
@@ -2,7 +2,7 @@ $(function () {
 
   let url_base = "/api/v1.1";
 
-  // preenche os campos de seleção
+  // preenche os campos de seleção: local, sensor, parametro
   fetch(`${url_base}/estacao`)
   .then(function(response) {
     var contentType = response.headers.get('content-type');
@@ -12,7 +12,7 @@ $(function () {
       .then(function(json) {
         $('#local-sel').empty();
 
-        estacoes = json.resources;
+        let estacoes = json.resources;
         estacoes.forEach(function(estacao) {
           $('#local-sel')
             .append($('<option></option>')
@@ -21,7 +21,8 @@ $(function () {
             );
         });
 
-        fetch(`${url_base}/estacao/${estacoes[0].id}/sensor`)
+        let estacao = estacoes[0];
+        fetch(`${url_base}/estacao/${estacao.id}/sensor`)
         .then(function(response) {
           var contentType = response.headers.get('content-type');
           if (contentType && contentType.indexOf("application/json") !== -1) {
@@ -30,7 +31,7 @@ $(function () {
             .then(function(json) {
               $('#sensor-sel').empty();
 
-              sensores = json.resources;
+              let sensores = json.resources;
               sensores.forEach(function(sensor) {
                 $('#sensor-sel')
                 .append($('<option></option>')
@@ -39,7 +40,8 @@ $(function () {
                 );
               });
 
-              fetch(`${url_base}/estacao/${estacoes[0].id}/sensor/${sensores[0].id}`)
+              let sensor = sensores[0];
+              fetch(`${url_base}/estacao/${estacao.id}/sensor/${sensor.id}`)
               .then(function(response) {
                 var contentType = response.headers.get('content-type');
                 if (contentType && contentType.indexOf("application/json") !== -1) {
@@ -56,6 +58,112 @@ $(function () {
                         .text(`${param}`)
                       );
                     });
+
+                    Highcharts.chart('container', {
+                      chart: {
+                        type: 'area',
+                        animation: Highcharts.svg, // don't animate in old IE
+                        marginRight: 10,
+                        events: {
+                          load: function () {
+
+                            let param = params[0];
+                            let serie = this.series[0];
+
+                            // set up the updating of the chart each second
+                            setInterval(function () {
+                              fetch(`${url_base}/sensor/${sensor.id}/${param}/last`)
+                              .then(function(response) {
+                                var contentType = response.headers.get('content-type');
+                                if (contentType && contentType.indexOf("application/json") !== -1) {
+
+                                  return response.json()
+                                  .then(function(json) {
+                                    // seconds (python) to milliseconds (js)
+                                    var x = json.resource.datahora * 1000;
+                                    var y = parseFloat(json.resource.valor);
+
+                                      console.log(param, x, y);
+                                      serie.addPoint([x, y], true, true);
+                                    });
+                                  }
+                                  });
+                              }, 1000);
+
+                            }
+                          }
+                        },
+
+                        time: {
+                            useUTC: false
+                        },
+
+                        title: {
+                            text: 'Live random data'
+                        },
+
+                        accessibility: {
+                            announceNewData: {
+                                enabled: true,
+                                minAnnounceInterval: 15000,
+                                announcementFormatter: function (allSeries, newSeries, newPoint) {
+                                    if (newPoint) {
+                                        return 'New point added. Value: ' + newPoint.y;
+                                    }
+                                    return false;
+                                }
+                            }
+                        },
+
+                        xAxis: {
+                            type: 'datetime',
+                            tickPixelInterval: 150
+                        },
+
+                        yAxis: {
+                            title: {
+                                text: 'Value'
+                            },
+                            plotLines: [{
+                                value: 0,
+                                width: 1,
+                                color: '#808080'
+                            }]
+                        },
+
+                        tooltip: {
+                            headerFormat: '<b>{series.name}</b><br/>',
+                            pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
+                        },
+
+                        legend: {
+                            enabled: true
+                        },
+
+                        exporting: {
+                            enabled: false
+                        },
+
+                        series: [{
+                          name: 'Random data',
+                          data: (function () {
+                            // generate an array of random data
+                            var data = [],
+                              time = (new Date()).getTime(),
+                              i;
+
+                            for (i = -19; i <= 0; i += 1) {
+                              data.push({
+                                x: time + i * 1000,
+                                y: 0
+                            });
+                          }
+                          return data;
+                        }())
+                      }]
+
+                    });
+
                   });
                 }
               });
@@ -107,102 +215,6 @@ $(function () {
           });
         }
       });
-  });
-
-  Highcharts.chart('container', {
-    chart: {
-      type: 'area',
-      animation: Highcharts.svg, // don't animate in old IE
-      marginRight: 10,
-      events: {
-        load: function () {
-
-          var umidade = this.series[0];
-          var temperatura = this.series[1];
-
-          // set up the updating of the chart each second
-          setInterval(function () {
-            sensores.forEach(function (sensor) {
-              sensor.params.split(",")
-                .forEach(function(param) {
-                  fetch(`${url_base}/sensor/${sensor.id}/${param}/last`)
-                    .then(function(response) {
-                      var contentType = response.headers.get('content-type');
-
-                      if (contentType && contentType.indexOf("application/json") !== -1) {
-                        return response.json()
-
-                        .then(function(json) {
-                          // seconds (python) to milliseconds (js)
-                          var x = json.resource.datahora * 1000;
-                          var y = parseFloat(json.resource.valor);
-
-                          if (param === "temperatura")
-                            temperatura.addPoint([x, y], true, true);
-                          else if (param === "umidade")
-                            umidade.addPoint([x, y], true, true);
-                        });
-                      }
-                    });
-                });
-            });
-          }, 1000);
-
-        }
-      }
-    },
-
-    time: {
-        useUTC: false
-    },
-
-    title: {
-        text: 'Live random data'
-    },
-
-    accessibility: {
-        announceNewData: {
-            enabled: true,
-            minAnnounceInterval: 15000,
-            announcementFormatter: function (allSeries, newSeries, newPoint) {
-                if (newPoint) {
-                    return 'New point added. Value: ' + newPoint.y;
-                }
-                return false;
-            }
-        }
-    },
-
-    xAxis: {
-        type: 'datetime',
-        tickPixelInterval: 150
-    },
-
-    yAxis: {
-        title: {
-            text: 'Value'
-        },
-        plotLines: [{
-            value: 0,
-            width: 1,
-            color: '#808080'
-        }]
-    },
-
-    tooltip: {
-        headerFormat: '<b>{series.name}</b><br/>',
-        pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
-    },
-
-    legend: {
-        enabled: true
-    },
-
-    exporting: {
-        enabled: false
-    },
-
-    series: []
   });
 
 });

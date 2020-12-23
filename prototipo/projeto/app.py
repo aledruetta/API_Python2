885a591bcbdcd16dbbf01262920b0955b023a25a
Alejandro Druetta <aledruetta@gmail.com>
2020-12-23 11:51:45 -0300

    ADD: config & .env files for configurations

--- prototipo/projeto/app.py
+++ prototipo/projeto/app.py
@@ -6,16 +6,7 @@ from projeto.ext import admin, api, auth, db, jwt, site
 
 def create_app():
     app = Flask(__name__)
-
-    app.config[
-            "SQLALCHEMY_DATABASE_URI"] = "postgresql://" + \
-                    "flaskapi:flaskapi@localhost:5432/flaskapi"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SECRET_KEY"] = "super-secret-key"
-    app.config["JWT_AUTH_USERNAME_KEY"] = "email"
-    app.config["JWT_AUTH_URL_RULE"] = "/token"
-    app.config["JWT_EXPIRATION_DELTA"] = timedelta(seconds=86400)  # one day
-    app.config["FLASK_ADMIN_SWATCH"] = "sandstone"
+    app.config.from_object("projeto.config.DevConfig")
 
     db.init_app(app)
     api.init_app(app)
